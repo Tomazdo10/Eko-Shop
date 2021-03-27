3e9a42378dd056f24325df17fb21198e521c0cb0
Tomazdo10 <tomaz.dobnik@gmail.com>
2021-03-27 08:44:00 +0000

    setup enviroment variables and automatic deployment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 import dj_database_url
 
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7xvy%gs9ky(@v0+_%%3@^ijbo4z0gy(qg%u6jkmnozax(u8c@u'
+SECRET_KEY = os.environ.get('SECRET_KEY', '7xvy%gs9ky(@v0+_%%3@^ijbo4z0gy(qg%u6jkmnozax(u8c@u')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['f8c4ff5-django-eko-shop-app.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
@@ -75,16 +75,16 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
+# DATABASES = {
+#   'default': {
+#      'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': BASE_DIR / 'db.sqlite3',
+#   }
+# }
 
 
 DATABASES = {
-    'default': dj_database_url.parse('postgres://njzgbjccghgzoa:a741bbd8bcc6497ff6fc2bb8353c533e4a7b495d12892cd29ad8a5a7ddbf0934@ec2-54-145-102-149.compute-1.amazonaws.com:5432/dcb353b982noah')
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 # Password validation
